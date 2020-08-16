a65e92a479a6d0164a510065089b5c3120bcf2eb
BennEntterprise <kyle@bennentterprise.com>
2020-08-16 00:02:42 -0400

    add npm scripts for dev processes

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const PORT = 8080
+const PORT = 5000
 
 app.get('/', (req: any, res: any) => {
     res.send('Express + Typescript Server')
