58695b5c00a4ceaaecf62d4fdb938ea714511246
BennEntterprise <kyle@bennentterprise.com>
2020-11-01 11:17:35 -0500

    add port for server file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-var PORT = 5000;
+var PORT = process.env.PORT || 5000;
 var path = require('path');
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(__dirname + '/client/build'));
     app.get('/', function (req, res) {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
